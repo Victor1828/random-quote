ad127d56a78f12704f3589c2307ac872ce36afdc
Victor Vega <victorvega1828@gmail.com>
2017-12-19 21:09:17 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { QuoteService } from '../services/quote.service';
+import { QuoteBoxComponent } from '../quote-box/quote-box.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [QuoteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/models/quote.class.ts
@@ -0,0 +1,7 @@
+export class Quote {
+
+    constructor(public quote: string, public author: string) {
+        this.quote = quote;
+        this.author = author;
+    }
+}
